09ccd6770656328b6873d4a0eecc56edd2599ce6
chun man poon <chunman906@hotmail.com>
2022-05-25 22:03:32 +0100

    created model and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts "Cleaning database.."
+Restaurant.destroy_all
+
+choice = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Creating restaurants.."
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::ProgrammingLanguage.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: choice.sample
+  )
+  restaurant.save!
+end
+puts "done! "
